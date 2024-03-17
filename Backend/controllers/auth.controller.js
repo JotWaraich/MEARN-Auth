7590ac37cced1d436f181b466b6b6577394b9495
Jot Waraich <gurjagjot@gmail.com>
2024-03-17 10:24:28 -0600

    create sign in route

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandle } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -10,6 +11,33 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: "Signup success" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const valdiUser = await User.findOne({ email });
+    if (!valdiUser) {
+      return next(errorHandle(404, "User not found"));
+    }
+    const isMatch = bcryptjs.compareSync(password, valdiUser.password);
+    if (!isMatch) {
+      return next(errorHandle(401, "Invalid username or password"));
+    }
+    const token = jwt.sign({ id: valdiUser._id }, process.env.JWT_SECRET);
+    // bnecause the _doc contains the password and we don't want to send the password to the client
+    const { password: hashedPassword, ...rest } = valdiUser._doc;
+    const expiryData = new Date(Date.now() + 3600000); // 1 hour
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expires: expiryData,
+      })
+      .status(200)
+      .json({ rest });
   } catch (error) {
     next(error);
   }
